90df2e152ff172a3cb25e543f15db68968f6b73e
Wasif Khan <waasif.khaan@gmail.com>
2018-08-31 00:44:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/' do 
+	"Whatsup"
+	end 
+	
+	get '/hello' do
+		erb :hello
 	end
 
+  get '/goodbye' do
+		erb :goodbye
+	end
+	
+	get '/date' do
+		erb :date
+	end
 
 end
